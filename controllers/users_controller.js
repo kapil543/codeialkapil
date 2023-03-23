5d6e1390e048b787ff26096f8c4f6287f8cde73d
kapil <kapil07092000@gmail.com>
2023-03-23 12:00:25 +0530

    setup staticfiles access for layouts and pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,23 @@
  module.exports.profile=function(req,res){
     res.end("<h1>User profile</h1>");
  };
+//  render the sign up page
+ module.exports.signUp=function(req,res){
+   return res.render("user_sign_up",{
+      title:"Codeial | Sign Up"
+   });
+ };
+//  render the sign in page
+ module.exports.signIn=function(req,res){
+   return res.render("user_sign_in",{
+      title:"Codeial | Sign In"
+   });
+ };
+//  get the sign up data
+module.exports.create=function(req,res){
+//   todo 
+};
+//  sign in and create a session for the user
+module.exports.createSession=function(req,res){
+   //   todo 
+   };
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express=require("express");
 const app=express();
 const port=8000;
+// note:use layout before route
+const expressLayouts=require("express-ejs-layouts");
+app.use(expressLayouts);
+// note:use static-file before route
+app.use(express.static("./assets"));
 // use express router
 app.use("/",require("./routes"));
 // set up the view engine
-app.set("view engine","ejs");
+app.set("view engine","ejs"); 
 app.set("views","./views");
+app.use(express.static("assets"));
+//  extract style and scripts from sub page into the layout
+app.set("layout extractStyles",true);
+app.set("layout extractScripts",true);
 
 
 app.listen(port,function(err){
